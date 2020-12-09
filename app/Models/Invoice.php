1fd5e8e299c3513d22359ad1d78c69be36da882f
Ibnanjung <anggayouman@gmail.com>
2020-12-09 13:52:04 +0700

    tambah trait HasFactory pada semua model

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Invoice extends Model
 {
 
+  use HasFactory;
+  
   /**
    * one to one invers with checkout
    * 
