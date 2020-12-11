ec85e86b98a6b50f033ef34e0c846a67bfd42caf
Ibnanjung <anggayouman@gmail.com>
2020-12-11 16:01:32 +0700

    create middleware untuk guest

---
+++ app/Http/Controllers/GuestController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use App\Models\Guest;
+
+class GuestController extends Controller
+{
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+      //
+  }
+
+  public function storeNewGuest()
+  {
+
+    $guest = new Guest;
+    $guest->token = md5(\random_bytes(10));
+    $guest->save();
+
+    return response()->json([
+      'guest_token' => $guest->token,
+      'description' => 'gunakan token ini pada headers(x-guest-token) untuk setiap request'
+    ]);
+
+  }
+
+  public function isValidGuestToken(Request $request)
+  {
+
+    $guest = Guest::whereToken($request->header('x-guest-token'))->first();
+
+    return ($guest) ? true : false;
+
+  }
+}
---
+++ app/Http/Middleware/GuestMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Http\Controllers\GuestController;
+
+class GuestMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+      if((new GuestController)->isValidGuestToken($request)) {
+        
+        return $next($request);
+
+      } else{
+
+        return (new GuestController)->storeNewGuest();
+
+      }
+    }
+}
