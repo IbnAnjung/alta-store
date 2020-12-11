bc86e9bfea779d032b106a1daac1aea8a1f9c04f
Ibnanjung <anggayouman@gmail.com>
2020-12-11 16:05:04 +0700

    tambah fitur lihat category dan category products

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * show all categories
+     * 
+     * @param Illuminate\Http\Request $request
+     * 
+     * @return Response
+     */
+    public function getAll(Request $request)
+    {
+
+      $this->validate($request, [
+        'page' => 'int',
+        'limit' => 'int',
+      ]);
+      
+      $limit = $request->get('limit');
+      $page  = $request->get('page', 1);
+
+      $totalCategory = Category::count();
+      $categories = Category::when($limit, function($categories) use($limit, $page){
+        $categories->skip(($page-1)*$limit)->take($limit);
+      })
+      ->get();
+
+      return response()->json([
+        'status'     => 'success', 
+        'data'       => $categories,
+        'total_data' => $totalCategory,
+        'page'       => $page,
+      ], 200);
+
+    }
+    
+    /**
+     * show all products category
+     * 
+     * @param int $idCategory
+     * @param Illuminate\Http\Request $request
+     * 
+     * @return Response
+     */
+    public function getCategoryProducts(Request $request, $idCategory)
+    {
+
+      $this->validate($request, [
+        'limit' => 'int', 
+        'page' => 'int'
+      ]);
+      
+      $limit = $request->get('limit');
+      $page  = $request->get('page', 1);
+
+      $category = Category::findOrFail($idCategory);
+      $totalProducts = $category->products->count();
+      $products = $category->products()
+        ->when($limit, function($categories) use($limit, $page){
+          $categories->skip(($page-1)*$limit)->take($limit);
+        })
+        ->get();
+      
+      return response()->json([
+        'status'     => 'success', 
+        'data'       => $products,
+        'total_data' => $totalProducts,
+        'page'       => $page,
+      ], 200);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,13 @@
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
-*/
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+*/
+
+$router->group(['prefix' => 'category', 'middleware' => 'guest'], function() use($router){
+    $router->get('/', ['as' => '', 'uses' => 'CategoryController@getAll']);
+    $router->get('{idCategory}/products', ['as' => '.products', 'uses'=> 'CategoryController@getCategoryProducts']);
 });
