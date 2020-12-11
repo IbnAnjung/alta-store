e0b130438039bdf50294abc81aad01edca55f911
Ibnanjung <anggayouman@gmail.com>
2020-12-11 16:03:55 +0700

    add hidden field creted_at & updated_at

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,6 +11,15 @@ class Category extends Model
   use HasFactory;
   
   /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+      'created_at', 'updated_at'
+    ];
+
+  /**
    * has many product
    * 
    * @return Eloquent
