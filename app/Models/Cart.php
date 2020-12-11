b155f13c935911359606177bdba1036d9a4d88e7
Ibnanjung <anggayouman@gmail.com>
2020-12-12 00:12:31 +0700

    add pivot property to products

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -18,7 +18,8 @@ class Cart extends Model
   public function products()
   {
 
-    return $this->belongsToMany(Product::class);
+    return $this->belongsToMany(Product::class)
+      ->withPivot(['qty']);
 
   }
 
